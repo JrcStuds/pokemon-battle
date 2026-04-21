084bfa6f7921ff8dec519b198ae58ddb714e6cee
Jacob Chamberlain <jacob.ryan.chamberlain@gmail.com>
2026-04-21 21:57:22 +1000

    Simple Battle Mechanics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import json, math
 
 
 
@@ -8,41 +8,72 @@ with open("moves.json", "r") as file:
     moves_db = json.load(file)
 
 
+
+class Move():
+    def __init__(self, name):
+        self.name = name
+        self.type = moves_db[name]["type"]
+        self.category = moves_db[name]["category"]
+        self.accuracy = moves_db[name]["accuracy"]
+        self.pp = moves_db[name]["pp"]
+        self.power = moves_db[name]["power"]
+
+
+    def execute(self, user, target):
+        atk_stat = user.attack if self.category == "physical" else user.sp_attack
+        def_stat = target.defense if self.category == "physical" else target.sp_defense
+        target.hp -= (((2/5)+2)*self.power*(atk_stat/def_stat))/50+2
+
+
+
 class Pokemon():
     def __init__(self, name):
         self.name = name
-        self.hp = pokemon_db[name]["hp"]
         self.type = pokemon_db[name]["type"]
+        self.hp = pokemon_db[name]["hp"]
         self.attack = pokemon_db[name]["attack"]
         self.defense = pokemon_db[name]["defense"]
-        self.moveset = pokemon_db[name]["moveset"]
+        self.sp_attack = pokemon_db[name]["sp_attack"]
+        self.sp_defense = pokemon_db[name]["sp_defense"]
+        self.speed = pokemon_db[name]["speed"]
+        self.moveset = [Move(move) for move in pokemon_db[name]["moveset"]]
+
+
+
+class Battler():
+    def __init__(self, pokemon: list):
+        self.pokemon = [Pokemon(name) for name in pokemon]
 
 
 
 class Battle():
     def __init__(self):
-        self.player_one = Pokemon("charmander")
-        self.player_two = Pokemon("bulbasaur")
-
-
-    def move(self, attacker: str, defender: str, move_idx: int):
-        attacker = self.player_one if attacker == "player_one" else self.player_two
-        defender = self.player_one if defender == "player_one" else self.player_two
-
-        if len(attacker.moveset) <= move_idx:
-            return "invalid move index"
-        
-        move = moves_db[attacker.moveset[move_idx]]
-
-        defender.hp -= move["power"]
-        return
+        self.player_one = Battler(pokemon=["Charmander"])
+        self.player_two = Battler(pokemon=["Bulbasaur"])
 
 
 
 battle = Battle()
 
-print(battle.player_one.name, battle.player_one.hp, battle.player_two.name, battle.player_two.hp)
+turn = 1
+running = True
+while running:
+    attacker = battle.player_one.pokemon[0] if turn == 1 else battle.player_two.pokemon[0]
+    defender = battle.player_one.pokemon[0] if turn == 2 else battle.player_two.pokemon[0]
 
-battle.move("player_one", "player_two", 0)
 
-print(battle.player_one.name, battle.player_one.hp, battle.player_two.name, battle.player_two.hp, "move used:", battle.player_one.moveset[0])
+    if round(attacker.hp) <= 0:
+        print(f"{defender.name} Wins!")
+        running = False
+        continue
+
+
+    print(f"Attacker: {attacker.name}, HP: {round(attacker.hp)}, Moves: {[move.name for move in attacker.moveset]}")
+    print(f"Defender: {defender.name}, HP: {round(defender.hp)}, Moves: {[move.name for move in defender.moveset]}")
+
+    move = int(input(f"which move would {attacker.name} like to execute? "))
+
+    attacker.moveset[move].execute(attacker, defender)
+    print(f"{attacker.name} used {attacker.moveset[move].name} on {defender.name}\n")
+
+    turn = 2 if turn == 1 else 1
