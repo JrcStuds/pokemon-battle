166806a9774f8324eb91afebabd74f5b1055fa44
jacob <jacob.ryan.chamberlain@gmail.com>
2026-04-24 14:56:44 +1000

    Attach Fight Menu To Pokemon

--- scripts/battle/battler.py
+++ scripts/battle/battler.py
@@ -5,11 +5,13 @@ import scripts.ui as ui
 
 
 class Battler(scenes.SceneBaseClass):
-    def __init__(self, rect: pygame.Rect, id: int, pokemon: list):
+    def __init__(self, battle, rect: pygame.Rect, id: int, pokemon: list):
         super().__init__(rect=rect)
+        self.battle = battle
 
         self.id = id
         self.pokemon = pokemon
+        self.active_pokemon = self.pokemon[0]
 
         self.add_elements(
             ui.Text((0, self.id*20+20), self.pokemon[0].name),
